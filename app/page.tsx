f7accdd7d4ee12b99905e210b56d3300e0c2ff4d
andres-pb <andres-pb10@hotmail.com>
2024-03-26 13:36:26 -0500

    page tunning

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,21 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
+     {
+      messages.map((m) => (
+        <div
+          key={m.id}
+          className={`whitespace-pre-wrap ${
+            m.role === "user"
+              ? "bg-green-700 p-3 m-2 rounded-lg"
+              : "bg-slate-700 p-3 m-2 rounded-lg"
+          }`}
+        >
+          {m.role === "user" ? "User: " : "AI: "}
           {m.content}
         </div>
-      ))}
- 
+      ))
+    }
       <form onSubmit={handleSubmit}>
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
